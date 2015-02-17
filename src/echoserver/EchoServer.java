ef3c69a0598456b983e9de227255c8ad5474a3ec
jonathan <jona_egebak@hotmail.dk>
2015-02-17 11:10:52 +0100

    tilføjet  arrayliste og removehandler

--- src/echoserver/EchoServer.java
+++ src/echoserver/EchoServer.java
@@ -5,6 +5,7 @@ import java.io.PrintWriter;
 import java.net.InetSocketAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayList;
 import java.util.Properties;
 import java.util.Scanner;
 import java.util.logging.Level;
@@ -18,10 +19,15 @@ public class EchoServer {
   private static boolean keepRunning = true;
   private static ServerSocket serverSocket;
   private static final Properties properties = Utils.initProperties("server.properties");
- 
+ ArrayList<handleClient> clients = new ArrayList<handleClient>();
 
   public static void stopServer() {
     keepRunning = false;
+  }
+  
+  public void  removeHandler(handleClient hc)
+  {
+      clients.remove(hc);
   }
 
   
@@ -33,6 +39,7 @@ public class EchoServer {
     String ip = properties.getProperty("serverIp");
     
     Logger.getLogger(EchoServer.class.getName()).log(Level.INFO, "Sever started. Listening on: "+port+", bound to: "+ip);
+   
     try {
       serverSocket = new ServerSocket();
       serverSocket.bind(new InetSocketAddress(ip, port));
@@ -40,6 +47,8 @@ public class EchoServer {
         Socket socket = serverSocket.accept(); //Important Blocking call
         Logger.getLogger(EchoServer.class.getName()).log(Level.INFO, "Connected to a client");        
         new handleClient(socket).start();
+        handleClient hc = new handleClient(socket);
+        clients.add(hc);
       } while (keepRunning);
     } 
     catch (IOException ex) {    
